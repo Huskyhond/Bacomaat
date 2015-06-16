bd9399293caa2d22d0c10924fb188aaca695eedd
Niek Eichner <niek1993@hotmail.com>
2015-06-16 14:52:50 +0200

    Print MOELAH BABY

--- Client/WebkitSerialEnzo/src/main/java/MLB/App.java
+++ Client/WebkitSerialEnzo/src/main/java/MLB/App.java
@@ -3,6 +3,15 @@ package MLB;
 
 //import com.corundumstu1dio.socketio.SocketIOClient;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
 import jssc.SerialPortEventListener;
@@ -29,19 +38,62 @@ public class App
 	static int withdrawDigitCount=0;
 	static String afgerond="";
 	
+	public static void printMoney(int ma, int mb, int mc) {
+		//Setting java files process:
+		ArrayList<String> lines = new ArrayList<String>();
+		String line = null;
+		try {
+            File f1 = new File("./Lejos.java");
+            FileReader fr = new FileReader(f1);
+            BufferedReader br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                if (line.contains("int dispenserA"))
+                    line = "int dispenserA = " + ma + ";";
+                else if(line.contains("int dispenserB"))
+                	line = "int dispenserB = " + mb + ";";
+                else if(line.contains("int dispenserC"))
+                	line = "int dispenserC = " + mc + ";";
+                line = line + "\r\n";
+                lines.add(line);
+            }
+            fr.close();
+            br.close();
+
+            FileWriter fw = new FileWriter(f1);
+            BufferedWriter out = new BufferedWriter(fw);
+            for(String s : lines)
+                 out.write(s);
+            out.flush();
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+		
+		//Building process:
+		ProcessBuilder builder = new ProcessBuilder(
+	            "cmd.exe", "/c", "nxjc Lejos.java && nxj -r Lejos");
+        builder.redirectErrorStream(true);
+        try {
+	        Process p = builder.start();
+	        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	        while (true) {
+	            line = r.readLine();
+	            if (line == null) { break; }
+	            System.out.println(line);
+	        }
+        }
+        catch(IOException ie) {
+        	
+        	
+        }
+	}
+	
 	/**
 		* @param args the command line arguments
 	*/
 	public static void main(String[] args) 
 	{
-		try {
-			NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
-			NXTInfo x[] = nxtComm.search("PLAT2");
-			nxtComm.open(x[0]);
-		}
-		catch(Exception ex) {
-			System.out.println("NXJ Not Found.");
-		}
+	
 		//String rekeningnummer="MLBI0200000001"; //Deze zijn om mee te testen
 		//String withdrawAmount="10";
                 if(args.length > 0){
