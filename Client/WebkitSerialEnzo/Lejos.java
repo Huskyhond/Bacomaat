b582ffeb43703cf74486e74533ec03d4d5755ef3
Niek Eichner <niek1993@hotmail.com>
2015-06-23 16:08:49 +0200

    Lejos aangepast , KTU SENSOREN!!!

--- Client/WebkitSerialEnzo/Lejos.java
+++ Client/WebkitSerialEnzo/Lejos.java
@@ -5,52 +5,34 @@ import java.lang.*;
 public class Lejos extends Thread {
 
   public static void printBillA() {
-    UltrasonicSensor s = new UltrasonicSensor(SensorPort.S1);
-    boolean stopped = false;
     Motor.A.backward();
-    while(!stopped) {
-        int dist = s.getDistance();
-        if(dist < 11 || dist > 17) {
-            Motor.A.stop();
-            Motor.A.forward();
-            Delay.msDelay(500);
-            Motor.A.stop();
-            stopped = true;
-        }
-    }
+
+    Delay.msDelay(2200);
+    Motor.A.stop();
+    Motor.A.forward();
+    Delay.msDelay(1000);
+    Motor.A.stop();
+    
   }
 
    public static void printBillB() {
-    UltrasonicSensor s = new UltrasonicSensor(SensorPort.S2);
-    boolean stopped = false;
     Motor.B.backward();
-    while(!stopped) {
-        int dist = s.getDistance();
-        System.out.println(dist);
-        if(dist < 11 || dist > 17) {
-            Motor.B.stop();
-            Motor.B.forward();
-            Delay.msDelay(500);
-            Motor.B.stop();
-            stopped = true;
-        }
-    }
+
+    Delay.msDelay(2200);
+    Motor.B.stop();
+    Motor.B.forward();
+    Delay.msDelay(1000);
+    Motor.B.stop();
   }
 
    public static void printBillC() {
-    UltrasonicSensor s = new UltrasonicSensor(SensorPort.S3);
-    boolean stopped = false;
     Motor.C.backward();
-    while(!stopped) {
-        int dist = s.getDistance();
-        if(dist < 11 || dist > 17) {
-            Motor.C.stop();
-            Motor.C.forward();
-            Delay.msDelay(500);
-            Motor.C.stop();
-            stopped = true;
-        }
-    }
+
+    Delay.msDelay(2200);
+    Motor.C.stop();
+    Motor.C.forward();
+    Delay.msDelay(1000);
+    Motor.C.stop();
   }
 
   public static void main (String[] args) {
@@ -59,7 +41,6 @@ int dispenserA = 1;
 int dispenserB = 1;
 int dispenserC = 1;
 
-    System.out.println("Hallo kees!?");
     // Default engine speeds.
     Motor.A.setSpeed(250); 
     Motor.B.setSpeed(250);
@@ -80,5 +61,7 @@ int dispenserC = 1;
         dispenserC--;
     }
 
+    //Debugging
+    Button.waitForAnyPress();
   }
 }
